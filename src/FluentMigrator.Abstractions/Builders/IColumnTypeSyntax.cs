f58187ef7dfaf03b5cb812160970ca553d2133ea
fabien.menager <fabien.menager@am-creations.fr>
2025-04-02 20:32:13 +0200

    Added remark about the 0-28 range on AsDecimal

--- src/FluentMigrator.Abstractions/Builders/IColumnTypeSyntax.cs
+++ src/FluentMigrator.Abstractions/Builders/IColumnTypeSyntax.cs
@@ -133,6 +133,7 @@ namespace FluentMigrator.Builders
         /// </summary>
         /// <param name="size">The number of digits</param>
         /// <param name="precision">The number of digits after the comma</param>
+        /// <remarks><see cref="System.Decimal" /> type in .NET supports 28 digits of precision, and the default behavior of FluentMigrator is to match that.</remarks>
         /// <returns>The next step</returns>
         TNext AsDecimal(int size, [Range(0, 28)] int precision);
 
